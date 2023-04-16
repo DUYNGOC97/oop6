a4efddfa691297e6d3d68765db4893c5b357da2c
DUYNGOC97 <dinhphuongduyngocduno@gmail.com>
2023-04-16 20:35:20 +0700

    oop

---
+++ TestCircle.java
@@ -0,0 +1,10 @@
+package circle;
+
+public class TestCircle {
+    public static void main(String[] args){
+        Circle circle = new Circle(20);
+        System.out.println("radius is:"+circle.getRadius());
+        System.out.println("area is: "+circle.getArea());
+
+    }
+}
